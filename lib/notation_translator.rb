15a92009fd139f9e4b44da683f0f2de1f455584d
clairedotcom <claireacarson@gmail.com>
2021-11-02 13:16:11 -0400

    Delete unused methods from NotationTranslator module

--- lib/notation_translator.rb
+++ lib/notation_translator.rb
@@ -1,34 +1,5 @@
 module NotationTranslator
-  def notation_to_coordinates(input)
-    [decode_piece(input), decode_coords(input)]
-  end
-
-  def decode_piece(input)
-    letter = input[0]
-    Pawn if ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'].include?(letter)
-    decode_non_pawns(input)
-  end
-
-  def decode_non_pawns(input)
-    first = input[0]
-
-    case first
-    when 'K'
-      piece = King
-    when 'Q'
-      piece = Queen
-    when 'R'
-      piece = Rook
-    when 'B'
-      piece = Bishop
-    when 'N'
-      piece = Knight
-    end
-    piece
-  end
-
   def decode_coords(input)
-    # input.slice!(0) if input.length == 3
     x = 0
     letters = %w[a b c d e f g h]
 
