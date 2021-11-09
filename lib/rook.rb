9b4768f983c8c48c6c2610a3327d194049d5d37b
clairedotcom <claireacarson@gmail.com>
2021-11-08 20:05:39 -0500

    Divide rook #moves into fours lines and remove #adjacents

--- lib/rook.rb
+++ lib/rook.rb
@@ -9,23 +9,42 @@ class Rook
   end
 
   def moves
-    possible_moves = []
-    x = @position[0]
-    y = @position[1]
-
-    (7).downto(1) { |num| possible_moves << [x + num, y] }
-    (7).downto(1) { |num| possible_moves << [x, y + num] }
-    (7).downto(1) { |num| possible_moves << [x - num, y] }
-    (7).downto(1) { |num| possible_moves << [x, y - num] }
-
-    possible_moves.delete_if { |move| off_board?(move) }
+    [line1, line2, line3, line4].flatten(1)
   end
 
-  def adjacents
+  def line1
+    line = []
     x = @position[0]
     y = @position[1]
 
-    squares = [[x + 1, y], [x, y + 1], [x - 1, y], [x, y - 1]]
-    squares.delete_if { |square| off_board?(square) }
+    (1).upto(7) { |num| line << [x + num, y] }
+    line.delete_if { |move| off_board?(move) }
+  end
+
+  def line2
+    line = []
+    x = @position[0]
+    y = @position[1]
+
+    (1).upto(7) { |num| line << [x, y + num] }
+    line.delete_if { |move| off_board?(move) }
+  end
+
+  def line3
+    line = []
+    x = @position[0]
+    y = @position[1]
+
+    (1).upto(7) { |num| line << [x - num, y] }
+    line.delete_if { |move| off_board?(move) }
+  end
+
+  def line4
+    line = []
+    x = @position[0]
+    y = @position[1]
+
+    (1).upto(7) { |num| line << [x, y - num] }
+    line.delete_if { |move| off_board?(move) }
   end
 end
