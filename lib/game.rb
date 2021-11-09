fdf7f718f0d30bc18010116c177e5dba35e50f5a
clairedotcom <claireacarson@gmail.com>
2021-11-09 13:23:21 -0500

    Add methods to remove blocked rook moves

--- lib/game.rb
+++ lib/game.rb
@@ -31,12 +31,9 @@ class Game
       start = decode_coords(solicit_start_square)
       finish = decode_coords(solicit_finish_square)
 
-      piece_name = @current_player.get_piece_at(start)
-      piece = name_to_class(piece_name)
+      return [start, finish] if valid_move_for_piece?(start, finish)
 
-      return [start, finish] if valid_move_for_piece?(piece, finish)
-
-      puts "Invalid move for that #{piece}. Please try again."
+      puts 'Invalid move for that piece. Please try again.'
     end
   end
 
@@ -52,16 +49,25 @@ class Game
   end
 
   def solicit_finish_square
-    puts 'Which square would you like to move to (e.g. a4): '
+    puts 'Which square would you like to move to? (e.g. a4): '
 
     loop do
       square = gets.chomp
       return square if valid_coords?(square)
 
-      puts 'Invalid input. Please enter the square number and letter (e.g. f5): '
+      puts 'Invalid input. Please try again.'
     end
   end
 
+  def valid_move_for_piece?(start, finish)
+    possibilities = @current_player.get_moves_for_piece(start)
+    possibilities.delete_if { |square| occupied_by_same_color?(square) }
+    return true if remove_blocked_squares(possibilities, start, finish).include?(finish)
+
+    false
+  end
+
+  # user input error handling: is there a piece in that square?
   def piece_at?(square)
     return true if @current_player.set.any? { |piece| piece.position == square }
 
@@ -77,27 +83,124 @@ class Game
     @current_player = @current_player == @player1 ? @player2 : @player1
   end
 
-  def valid_move_for_piece?(piece, move)
-    return true if @current_player.move_possible?(piece, move)
-
+  def occupied_by_same_color?(square)
+    @current_player.set.each do |piece|
+      return true if piece.position == square
+    end
     false
   end
 
-  # def occupied_by_same_color?(square)
-  #   @current_player.set.each do |piece|
-  #     return true if piece.position == square
-  #   end
-  #   false
-  # end
+  def remove_blocked_squares(possibilities, start, finish)
+    piece = @current_player.get_piece_at(start)
+    piece_name = piece.class.name
 
-  # def occupied_by_opposite_color?(square)
-  #   player = @current_player == @player1 ? @player2 : @player1
+    case piece_name
+    when 'King'
+      possibilities
+    when 'Knight'
+      possibilities
+    when 'Rook'
+      rook_move_iterator(piece)
+    when 'Bishop'
+      bishop_move_iterator(possibilities, start, finish)
+    when 'Queen'
+      queen_move_iterator(possibilities, start, finish)
+    when 'Pawn'
+      possibilities.delete_if { |square| occupied_by_opposite_color?(square)}
+    end
+  end
 
-  #   player.set.each do |piece|
-  #     return true if piece.position == square
-  #   end
-  #   false
-  # end
+  def rook_move_iterator(piece)
+    result = []
+    result << rook_line1_iterator(piece)
+    result << rook_line2_iterator(piece)
+    result << rook_line3_iterator(piece)
+    result << rook_line4_iterator(piece)
+    result.flatten(1)
+  end
+
+  def rook_line1_iterator(piece)
+    result = []
+    path = piece.line1
+
+    path.each do |square|
+      if occupied_by_any_piece?(square)
+        break
+      else
+        result << square
+      end
+    end
+    result
+  end
+
+  def rook_line2_iterator(piece)
+    result = []
+    path = piece.line2
+
+    path.each do |square|
+      if occupied_by_any_piece?(square)
+        break
+      else
+        result << square
+      end  
+    end
+    result
+  end
+
+  def rook_line3_iterator(piece)
+    result = []
+    path = piece.line3
+
+    path.each do |square|
+      if occupied_by_any_piece?(square)
+        break
+      else
+        result << square
+      end  
+    end
+    result
+  end
+
+  def rook_line4_iterator(piece)
+    result = []
+    path = piece.line4
+
+    path.each do |square|
+      if occupied_by_any_piece?(square)
+        break
+      else
+        result << square
+      end  
+    end
+    result
+  end
+
+  def occupied_by_any_piece?(square)
+    if @player1.set.any? { |piece| piece.position == square }
+      true
+    elsif @player2.set.any? { |piece| piece.position == square }
+      true
+    else
+      false
+    end
+  end
+
+  def bishop_move_iterator(possible_moves, start, finish)
+    # add lines to Bishop class first
+  end
+
+  def queen_move_iterator(possible_moves, start, finish)
+    # combine bishop & rook iterators?
+  end
+
+  def occupied_by_opposite_color?(square)
+    player = @current_player == @player1 ? @player2 : @player1
+
+    player.set.each do |piece|
+      return true if piece.position == square
+    end
+    false
+  end
 
   private
 
