95e1a39cec12438c190633bb76dfa1567681d895
clairedotcom <claireacarson@gmail.com>
2021-11-09 19:42:44 -0500

    Break Queen #moves into eight lines

--- lib/queen.rb
+++ lib/queen.rb
@@ -19,13 +19,43 @@ class Queen
     possible_moves << dummy_bishop.moves
   end
 
-  def adjacents
-    squares = []
+  def line1
+    rook_line1 = Rook.new(@position)
+    rook_line1.line1
+  end
 
-    dummy_rook = Rook.new(@position)
-    dummy_bishop = Bishop.new(@position)
+  def line2
+    rook_line2 = Rook.new(@position)
+    rook_line2.line2
+  end
 
-    squares << dummy_rook.adjacents
-    squares << dummy_bishop.adjacents
+  def line3
+    rook_line3 = Rook.new(@position)
+    rook_line3.line3
+  end
+
+  def line4
+    rook_line4 = Rook.new(@position)
+    rook_line4.line4
+  end
+
+  def line5
+    bishop_line1 = Bishop.new(@position)
+    bishop_line1.line1
+  end
+
+  def line6
+    bishop_line2 = Bishop.new(@position)
+    bishop_line2.line2
+  end
+
+  def line7
+    bishop_line3 = Bishop.new(@position)
+    bishop_line3.line3
+  end
+
+  def line8
+    bishop_line4 = Bishop.new(@position)
+    bishop_line4.line4
   end
 end
