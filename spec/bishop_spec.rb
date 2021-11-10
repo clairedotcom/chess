b1649562c6d219d5ebd0e631acc0419f60a4deae
clairedotcom <claireacarson@gmail.com>
2021-11-09 19:01:42 -0500

    Update bishop_spec.rb to reflect changes in Bishop class

--- spec/bishop_spec.rb
+++ spec/bishop_spec.rb
@@ -1,18 +1,46 @@
 require_relative '../lib/bishop'
 
 describe Bishop do
-  describe '#moves' do
-    context 'when the bishop is at [4,4]' do
-      subject(:test_bishop) { described_class.new([4, 4]) }
+  describe '#line1' do
+    subject(:bishop_c1) { described_class.new([2, 0]) }
 
-      it 'generates [5,5] as a move' do
-        all_moves = test_bishop.moves
-        expect(all_moves).to include([5, 5])
+    context 'when the bishop is at an initial position of c1' do
+      it 'returns the correct array of moves' do
+        result = [[3, 1], [4, 2], [5, 3], [6, 4], [7, 5]]
+        expect(bishop_c1.line1).to eq(result)
       end
+    end
+  end
 
-      it 'does not generate [8,8] as a move' do
-        all_moves = test_bishop.moves
-        expect(all_moves).not_to include([8, 8])
+  describe '#line2' do
+    subject(:bishop_c1) { described_class.new([2, 0]) }
+
+    context 'when the bishop is at c1' do
+      it 'returns the correct array of moves' do
+        result = [[1, 1], [0, 2]]
+        expect(bishop_c1.line2).to eq(result)
+      end
+    end
+  end
+
+  describe '#line3' do
+    subject(:bishop_c1) { described_class.new([2, 0]) }
+
+    context 'when the bishop is at c1' do
+      it 'returns the correct array of moves' do
+        result = []
+        expect(bishop_c1.line3).to eq(result)
+      end
+    end
+  end
+
+  describe '#line4' do
+    subject(:bishop_c1) { described_class.new([2, 0]) }
+
+    context 'when the bishop is at c1' do
+      it 'returns the correct array of moves' do
+        result = []
+        expect(bishop_c1.line4).to eq(result)
       end
     end
   end
