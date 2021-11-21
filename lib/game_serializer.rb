9c90c115f227d0ce576872dbb6a569df0edb469d
clairedotcom <claireacarson@gmail.com>
2021-11-20 20:28:17 -0500

    Add #save_game, #save_game_to_yaml, #count_files to GameSerializer

--- lib/game_serializer.rb
+++ lib/game_serializer.rb
@@ -3,10 +3,23 @@ require 'yaml'
 module GameSerializer
   def save_game
     Dir.mkdir 'game_archive' unless Dir.exist? 'game_archive'
-    #name files game_1, game_2, etc
+    filename = "game_#{count_files}.yaml"
+    File.open("/game_archive/#{filename}", 'w') { |file| file.write(save_game_to_yaml)}
   end
 
   def save_game_to_yaml
+    YAML.dump(
+      'player1' => @player1,
+      'player2' => @player2,
+      'board' => @board,
+      'current_player' => @current_player
+    )
+  end
 
-  end  
+  def count_files
+    return 0 if Dir.empty?('/game_archive')
+
+    dir = '/game_archive'
+    Dir[File.join(dir, '**', '*')].count { |file| File.file?(file) }
+  end
 end
