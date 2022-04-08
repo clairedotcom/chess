1220ac487a7d9f1339978be0d1f4fc7f5c6241e7
clairedotcom <claireacarson@gmail.com>
2022-04-07 20:31:48 -0400

    Replace DisplayGenerator with Display object and clean up methods

--- lib/board.rb
+++ lib/board.rb
@@ -1,8 +1,6 @@
-require_relative 'display_generator'
+require_relative 'display'
 
 class Board
-  include DisplayGenerator
-
   attr_accessor :display
   attr_reader :data
 
@@ -12,14 +10,7 @@ class Board
   end
 
   def create_display(white_set, black_set)
-    @display.each_with_index do |row, x|
-      row.each_with_index do |_col, y|
-        @display[x][y] = (x + y).even? ? colorize('black') : colorize('white')
-      end
-    end
-    place_white_pieces(white_set)
-    place_black_pieces(black_set)
-    print_board
+    Display.new(white_set, black_set).set_display
   end
 
   def set_initial_data
---
+++ lib/display.rb
@@ -0,0 +1,85 @@
+class Display
+  def initialize(white_set, black_set)
+    @white_set = white_set
+    @black_set = black_set
+    @display = Array.new(8) { Array.new(8, ' ') }
+  end
+
+  def set_display
+    colorize_empty_board
+    place_white_pieces(@white_set)
+    place_black_pieces(@black_set)
+    print_board
+  end
+
+  def place_white_pieces(set)
+    set.each do |piece|
+      x = piece.position[0]
+      y = piece.position[1]
+      name = piece.class.name.downcase
+      @display[y][x] = (x + y).even? ? black_bg(white_pieces(name)) : white_bg(white_pieces(name))
+    end
+  end
+
+  def place_black_pieces(set)
+    set.each do |piece|
+      x = piece.position[0]
+      y = piece.position[1]
+      name = piece.class.name.downcase
+      @display[y][x] = (x + y).even? ? black_bg(black_pieces(name)) : white_bg(black_pieces(name))
+    end
+  end
+
+  def print_board
+    letters = ['  a ', 'b', ' c', ' d', ' e', ' f', ' g', ' h']
+    numbers = [' ', '8 ', '7 ', '6 ', '5 ', '4 ', '3 ', '2 ', '1 ']
+
+    @display.unshift(letters)
+    @display.push([' a ', 'b ', 'c ', 'd ', 'e ', 'f ', 'g ', 'h'])
+
+    @display.reverse.each_with_index do |row, i|
+      puts "#{numbers[i]} #{row.join('')} #{numbers[i]}"
+    end
+
+    @display.pop(2)
+  end
+
+  def colorize_empty_board
+    @display.each_with_index do |row, x|
+      row.each_with_index do |_col, y|
+        @display[x][y] = (x + y).even? ? "\e[44m  \e[0m" : "\e[46m  \e[0m"
+      end
+    end
+  end
+
+  def black_bg(piece)
+    "\e[44m#{piece}\e[0m"
+  end
+
+  def white_bg(piece)
+    "\e[46m#{piece}\e[0m"
+  end
+
+  def white_pieces(piece)
+    {
+      'king' => "\e[37m\u265A \e[0m",
+      'queen' => "\e[37m\u265B \e[0m",
+      'rook' => "\e[37m\u265C \e[0m",
+      'bishop' => "\e[37m\u265D \e[0m",
+      'knight' => "\e[37m\u265E \e[0m",
+      'pawn' => "\e[37m\u265F \e[0m"
+    }[piece]
+  end
+
+  def black_pieces(piece)
+    {
+      'king' => "\e[30m\u265A \e[0m",
+      'queen' => "\e[30m\u265B \e[0m",
+      'rook' => "\e[30m\u265C \e[0m",
+      'bishop' => "\e[30m\u265D \e[0m",
+      'knight' => "\e[30m\u265E \e[0m",
+      'pawn' => "\e[30m\u265F \e[0m"
+    }[piece]
+  end
+end
+
