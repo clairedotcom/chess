bb4712808c746efe82f93784c025ae9cb1fd8949
cacarson <claireacarson@gmail.com>
2022-08-24 12:52:23 -0400

    Remove set and related methods from Player class

--- lib/player.rb
+++ lib/player.rb
@@ -9,7 +9,7 @@ require_relative 'castling'
 require_relative 'utilities'
 
 class Player
-  attr_reader :id, :set, :loser
+  attr_reader :id, :loser
 
   include Utilities
   include Dialogue
@@ -17,51 +17,7 @@ class Player
 
   def initialize(id)
     @id = id
-    @set = generate_set
     @loser = false
-  end
-
-  def generate_set
-    case @id
-    when :white
-      generate_white_set
-    when :black
-      generate_black_set
-    end
-  end
-
-  def generate_white_set
-    set = []
-    set << generate_white_pawns
-    set << [Rook.new([0, 0], :white), Rook.new([7, 0], :white)]
-    set << [Knight.new([1, 0], :white), Knight.new([6, 0], :white)]
-    set << [Bishop.new([2, 0], :white), Bishop.new([5, 0], :white)]
-    set << [Queen.new([3, 0], :white)]
-    set << [King.new([4, 0], :white)]
-    set.flatten
-  end
-
-  def generate_black_set
-    set = []
-    set << generate_black_pawns
-    set << [Rook.new([0, 7], :black), Rook.new([7, 7], :black)]
-    set << [Knight.new([1, 7], :black), Knight.new([6, 7], :black)]
-    set << [Bishop.new([2, 7], :black), Bishop.new([5, 7], :black)]
-    set << [Queen.new([3, 7], :black)]
-    set << [King.new([4, 7], :black)]
-    set.flatten
-  end
-
-  def generate_white_pawns
-    pawns = []
-    (0).upto(7) { |x| pawns << Pawn.new([x, 1], :white) }
-    pawns
-  end
-
-  def generate_black_pawns
-    pawns = []
-    (0).upto(7) { |x| pawns << Pawn.new([x, 6], :black) }
-    pawns
   end
 
   def input_start_square
@@ -89,35 +45,6 @@ class Player
       end
 
       puts invalid_input_message
-    end
-  end
-
-  def same_color?(square)
-    @set.any? { |piece| piece.position == square } ? true : false
-  end
-
-  def update_set(start, finish)
-    get_piece_at(start).position = finish
-  end
-
-  def get_moves_for_piece(start)
-    get_piece_at(start).moves
-  end
-
-  def get_piece_at(square)
-    @set.select { |piece| piece.position == square }.first
-  end
-
-  def find_king_location
-    @set.select { |piece| piece.instance_of? King }.first.position
-  end
-
-  def delete_piece(square)
-    @set.pieces.each do |piece|
-      if piece.position == square
-        @set.delete(piece)
-        @loser = true if piece.instance_of? King
-      end
     end
   end
 
