347961a94061c0691e03156e1def4655bcf8e13b
cacarson <claireacarson@gmail.com>
2023-05-13 10:26:21 -0400

    Add move count field to Piece class

--- lib/piece.rb
+++ lib/piece.rb
@@ -1,4 +1,5 @@
-# Super class of individual piece types. Contains shared behavior
+# Super class of individual piece types. Contains data that is common to
+# all piece classes.
 
 class Piece
   attr_reader :color
@@ -7,5 +8,6 @@ class Piece
   def initialize(position, color)
     @position = position
     @color = color
+    @move_count = 0
   end
 end
