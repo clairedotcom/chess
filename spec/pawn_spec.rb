d66cc5a95b8a9a729d89e69d48deb3754383e5c8
cacarson <claireacarson@gmail.com>
2023-06-06 19:01:44 -0400

    Add test for set_icon to Pawn spec

--- spec/pawn_spec.rb
+++ spec/pawn_spec.rb
@@ -1,13 +1,21 @@
 require_relative '../lib/pawn'
 
 describe Pawn do
-  subject(:test_pawn) { described_class.new([0, 1], :white) }
+  subject(:white_pawn) { described_class.new([0, 1], :white) }
+  subject(:black_pawn) { described_class.new([6, 4], :black) }
 
   describe '#set_icon' do
     context 'when a white pawn is initialized' do
       it 'returns the correct icon string' do
         icon_string = "\e[37m\u265F \e[0m"
-        expect(test_pawn.icon).to eq(icon_string)
+        expect(white_pawn.icon).to eq(icon_string)
+      end
+    end
+
+    context 'when a black pawn is initialized' do
+      it 'returns the corret icon string' do
+        icon_string = "\e[30m\u265F \e[0m"
+        expect(black_pawn.icon).to eq(icon_string)
       end
     end
   end
