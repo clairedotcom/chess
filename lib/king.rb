9ce7c2e3b7306b902733f4d7847a5f55ccf41c71
cacarson <claireacarson@gmail.com>
2023-08-14 14:48:54 -0400

    Remove unused code and tests for King pieces

--- lib/king.rb
+++ lib/king.rb
@@ -15,21 +15,4 @@ class King < Piece
   def move_set
     [[0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1], [-1, 0], [-1, 1]]
   end
-
-  def moves
-    possible_moves = []
-    x = @position[0]
-    y = @position[1]
-
-    possible_moves << [x, y + 1]
-    possible_moves << [x + 1, y + 1]
-    possible_moves << [x + 1, y]
-    possible_moves << [x + 1, y - 1]
-    possible_moves << [x, y - 1]
-    possible_moves << [x - 1, y - 1]
-    possible_moves << [x - 1, y]
-    possible_moves << [x - 1, y + 1]
-
-    possible_moves.delete_if { |move| off_board?(move) }
-  end
 end
