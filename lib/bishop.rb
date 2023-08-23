86fe2e55e7409a16b6a43049f4c089b96b5f31d7
cacarson <claireacarson@gmail.com>
2023-08-23 19:32:33 -0400

    Implement and test #get_possible_moves for Bishop class

--- lib/bishop.rb
+++ lib/bishop.rb
@@ -15,4 +15,14 @@ class Bishop < Piece
   def move_set
     [[1, 1], [-1, 1], [-1, -1], [1, -1]]
   end
+
+  def get_possible_moves
+    squares = []
+    7.times do |i|
+      move_set.map do |step|
+        squares << [@position[0] + (i + 1)*step[0], @position[1] + (i + 1)*step[1]]
+      end
+    end
+    squares.delete_if { |square| !((0..7).include?(square[0]) && (0..7).include?(square[1])) }
+  end
 end
--- spec/bishop_spec.rb
+++ spec/bishop_spec.rb
@@ -11,4 +11,37 @@ describe Bishop do
       end
     end
   end
+
+  describe '#get_possible_moves' do
+    context 'when the white bishop at c1 is at its initial position' do
+      it 'returns the correct squares' do
+        bishop = Bishop.new([2, 0], :white)
+        correct_squares = [[1, 1], [0, 2], [3, 1], [4, 2], [5, 3], [6, 4], [7, 5]]
+      end
+    end
+
+    context 'when the white bishop at f1 is at its initial position' do
+      it 'returns the correct squares' do
+        bishop = Bishop.new([5, 0], :white)
+        correct_squares = [[6, 1], [7, 2], [4, 1], [3, 2], [2, 3], [1, 4], [0, 5]]
+        expect(bishop.get_possible_moves).to match_array(correct_squares)
+      end
+    end
+
+    context 'when the black bishop at c8 is at its initial position' do
+      it 'returns the correct squares' do
+        bishop = Bishop.new([2, 7], :black)
+        correct_squares = [[1, 6], [0, 5], [3, 6], [4, 5], [5, 4], [6, 3], [7, 2]]
+        expect(bishop.get_possible_moves).to match_array(correct_squares)
+      end
+    end
+
+    context 'when the white bishop at f8 is at its initial position' do
+      it 'returns the correct squares' do
+        bishop = Bishop.new([5, 7], :black)
+        correct_squares = [[6, 6], [7, 5], [4, 6], [3, 5], [2, 4], [1, 3], [0, 2]]
+        expect(bishop.get_possible_moves).to match_array(correct_squares)
+      end
+    end
+  end
 end
